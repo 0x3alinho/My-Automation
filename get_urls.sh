41ebadf557855dd60f18946a2dbe8abe0b5095e8
Ali Karam <106202881+0x3alinho@users.noreply.github.com>
2023-12-15 21:44:31 +0200

    Update get_urls.sh

--- get_urls.sh
+++ get_urls.sh
@@ -3,7 +3,9 @@ cat alive_subs.txt | gau --threads 5 --blacklist png,jpg,gif --o gau_result.txt
 cat gau_result.txt >> url1.txt
 rm gau_result.txt
 katana -list alive_links.txt >> url1.txt
+cat alive_links.txt | while read host; do waymore -i $host; done | anew more.txt
+cat more.txt | sort -u >> url1.txt
 cat url1.txt | sort -u >> urls.txt
-rm url1.txt
+rm url1.txt more.txt
 cat urls.txt | grep application/json >> app_json.txt
-                                                         
+cat urls | gauplus -subs | grep — -color -E. “.xls | \\. xml | \\.xlsx | \\.json | \\. pdf | \\.sql | \\. doc| \\.docx | \\. pptx| \\.txt| \\.zip| \\.tar.gz>
