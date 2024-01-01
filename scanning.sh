f81c65b0f877ea100f881f53ba261fe77acf9805
Ali Karam <106202881+0x3alinho@users.noreply.github.com>
2024-01-01 03:52:05 +0200

    Update scanning.sh

--- scanning.sh
+++ scanning.sh
@@ -6,6 +6,6 @@ cat all_domains_ip.txt >> IP.txt
 cat IP.txt | sort -u >> ips.txt
 rm all_domains_ip.txt IP.txt
 
-masscann -p1-65535 -iL ips.txt -max-rate 10000 -oG masscan_result.txt
+masscan -p1-65535 -iL ips.txt -oG masscan_result.txt
 
 #nmap -O -sV -iL ips.txt -oN nmap_result.txt --script=vuln&
